6e9835706f175d855ab9af6ada5f70c4dd2742a4
aryaaa40 <haekalkunz40@gmail.com>
2026-03-16 16:44:57 +0700

    feat: complete monorepo structure with frontend and backend

---
+++ backend/src/main/java/com/example/SpringEventDrivent/SpringEventDriventApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringEventDrivent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventDriventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEventDriventApplication.class, args);
+	}
+
+}
